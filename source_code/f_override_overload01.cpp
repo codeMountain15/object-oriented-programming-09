72e535bb68b1b089929a156ee4527b135ccd787e
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-12 14:10:34 +0200

    Create f_override_overload01.cpp

---
+++ source_code/f_override_overload01.cpp
@@ -0,0 +1,37 @@
+// f_override_overload01.cpp
+//
+
+#include<iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void speak() { cout << "The Animal speaks\n"; }
+
+    void sing() { cout << "The Animal sings\n"; }
+};
+
+class Bird : public Animal {
+public:
+    // function overriding
+    void speak() { cout << "The Bird speaks\n"; }
+
+    // function overloading
+    using Animal::sing;     // compile error without this code
+                            //void sing() { Animal::sing(); } // <- alternately
+    void sing(int a) { for (int i = 0; i < a; i++) cout << "The Bird sings\n"; }
+};
+
+
+
+int main() {
+
+    Bird parrot1;
+
+    parrot1.speak();
+    parrot1.sing(3);
+    parrot1.sing();
+
+
+    return 0;
+}
