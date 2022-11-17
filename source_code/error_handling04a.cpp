59bcf341368c753bde03239e9ce789e62b9b7c49
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-17 19:30:09 +0200

    Create error_handling04a.cpp

---
+++ source_code/error_handling04a.cpp
@@ -0,0 +1,38 @@
+// error_handling04a.cpp
+// throw exception from 
+
+#include <iostream>
+#include <limits> // for function numeric_limits<T>::max()
+
+using namespace std;
+
+float divide(int a, int b){
+    if (b == 0) {
+        throw(invalid_argument("You should not divide anything with zero...\n"));
+    }
+    else return a / b;
+}
+
+int main()
+{
+    float input1, input2;
+
+    cout << "Give an integer:\n";
+    cin >> input1;
+    cout << "Give another integer to divide " << input1 << endl;
+    cin >> input2;
+
+    try
+    {
+        cout << "The result is: " << divide(input1, input2) << endl;
+    }
+
+    catch (invalid_argument& inpt)
+    {
+        cerr << inpt.what() << endl;
+    }
+
+    // continue code
+
+    return 0;
+}
